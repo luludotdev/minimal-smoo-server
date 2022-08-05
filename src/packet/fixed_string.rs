19bcbbde5fcd35ae7c9ee413327eefc08333bf29
Jack Baron <jackmbaron@gmail.com>
2022-08-05 18:29:40 +0100

    implement parsing for fixedstring

--- src/packet/fixed_string.rs
+++ src/packet/fixed_string.rs
@@ -1,6 +1,8 @@
 use std::fmt::Display;
-use std::str::Utf8Error;
+use std::str::{FromStr, Utf8Error};
 
+use bytes::{BufMut, BytesMut};
+use color_eyre::eyre::eyre;
 use color_eyre::Result;
 
 use super::PacketBytes;
@@ -13,7 +15,7 @@ pub struct FixedString<const N: usize> {
 impl<const N: usize> FixedString<N> {
     #[inline]
     pub fn try_to_string(&self) -> Result<&str, Utf8Error> {
-        std::str::from_utf8(&self.inner)
+        std::str::from_utf8(&self.inner).map(|s| s.trim_matches('\0'))
     }
 }
 
@@ -34,5 +36,30 @@ impl<const N: usize> PacketBytes for FixedString<N> {
     fn from_bytes(buf: &mut bytes::Bytes) -> Result<Self> {
         let inner = <[u8; N] as PacketBytes>::from_bytes(buf)?;
         Ok(Self { inner })
+    }
+}
+
+impl<const N: usize> FromStr for FixedString<N> {
+    type Err = color_eyre::Report;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let string = s.to_owned();
+        let mut bytes = BytesMut::from(string.as_bytes());
+
+        let max_len = std::mem::size_of::<[u8; N]>();
+        let len = bytes.len();
+
+        if len > max_len {
+            Err(eyre!("string is too long"))
+        } else {
+            // Pad
+            let pad_len = max_len - len;
+
+            let padding = vec![0; pad_len];
+            bytes.put(&padding[..]);
+
+            let inner = bytes[..].try_into().unwrap();
+            Ok(Self { inner })
+        }
     }
 }
