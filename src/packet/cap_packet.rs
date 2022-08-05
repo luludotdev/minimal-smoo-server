8d4e0d00aa16ee7c3b24cdc06344722d1649713c
Jack Baron <jackmbaron@gmail.com>
2022-08-05 19:04:12 +0100

    use a fixedstring for cap packets

--- src/packet/cap_packet.rs
+++ src/packet/cap_packet.rs
@@ -1,6 +1,7 @@
 use glam::{Quat, Vec3};
 use smoo_derive::PacketBytes;
 
+use super::fixed_string::FixedString;
 use super::header::PacketType;
 use super::traits::Packet;
 
@@ -9,7 +10,7 @@ pub struct CapPacket {
     pub position: Vec3,
     pub quaternion: Quat,
     pub cap_out: bool,
-    pub cap_anim: [u8; 30],
+    pub cap_anim: FixedString<0x30>,
 }
 
 impl From<CapPacket> for PacketType {
