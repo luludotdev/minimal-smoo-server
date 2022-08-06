20dd1245aeee85213608e1abfaa67b19b406d4c0
Jack Baron <jackmbaron@gmail.com>
2022-08-06 13:56:33 +0100

    convert fixed string into string

--- src/packet/fixed_string.rs
+++ src/packet/fixed_string.rs
@@ -3,7 +3,7 @@ use std::str::{FromStr, Utf8Error};
 
 use bytes::{Buf, BufMut, BytesMut};
 use color_eyre::eyre::eyre;
-use color_eyre::Result;
+use color_eyre::{Report, Result};
 
 use super::PacketBytes;
 
@@ -14,14 +14,19 @@ pub struct FixedString<const N: usize> {
 
 impl<const N: usize> FixedString<N> {
     #[inline]
-    pub fn try_to_string(&self) -> Result<&str, Utf8Error> {
+    pub fn try_as_str(&self) -> Result<&str, Utf8Error> {
         std::str::from_utf8(&self.inner).map(|s| s.trim_matches('\0'))
+    }
+
+    #[inline]
+    pub fn try_into_string(&self) -> Result<String, Utf8Error> {
+        Ok(self.try_as_str()?.to_owned())
     }
 }
 
 impl<const N: usize> Display for FixedString<N> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let str = self.try_to_string().unwrap();
+        let str = self.try_as_str().unwrap();
         write!(f, "{str}")
     }
 }
@@ -40,7 +45,7 @@ impl<const N: usize> PacketBytes for FixedString<N> {
 }
 
 impl<const N: usize> FromStr for FixedString<N> {
-    type Err = color_eyre::Report;
+    type Err = Report;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let string = s.to_owned();
@@ -61,5 +66,14 @@ impl<const N: usize> FromStr for FixedString<N> {
             let inner = bytes[..].try_into().unwrap();
             Ok(Self { inner })
         }
+    }
+}
+
+impl<const N: usize> TryFrom<FixedString<N>> for String {
+    type Error = Utf8Error;
+
+    #[inline]
+    fn try_from(value: FixedString<N>) -> Result<Self, Self::Error> {
+        value.try_into_string()
     }
 }
