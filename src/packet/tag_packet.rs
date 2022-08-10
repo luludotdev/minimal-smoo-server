b1fb311a3d7ee629f19cbdf63a39b771caf73b8e
Jack Baron <jackmbaron@gmail.com>
2022-08-10 21:08:14 +0100

    its a bitfield

--- src/packet/tag_packet.rs
+++ src/packet/tag_packet.rs
@@ -1,39 +1,10 @@
-use bytes::Buf;
-use color_eyre::eyre::eyre;
-use color_eyre::Result;
 use smoo_derive::Packet;
-
-use super::PacketBytes;
 
 #[derive(Debug, Clone, Copy, PartialEq, Packet)]
 #[packet("Tag")]
 pub struct TagPacket {
-    update_type: UpdateType,
+    update_bits: u8,
     is_it: bool,
     seconds: u8,
     minutes: u16,
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u8)]
-pub enum UpdateType {
-    Time = 1 << 0,
-    State = 1 << 1,
-}
-
-impl PacketBytes for UpdateType {
-    fn write_bytes(&self, buf: &mut bytes::BytesMut) -> usize {
-        let u8 = *self as u8;
-        u8.write_bytes(buf)
-    }
-
-    fn from_bytes<T: Buf>(buf: &mut T) -> Result<Self> {
-        let id = u8::from_bytes(buf)?;
-        match id {
-            1 => Ok(UpdateType::Time),
-            2 => Ok(UpdateType::State),
-
-            _ => Err(eyre!("invalid tag update type: {id}")),
-        }
-    }
 }
